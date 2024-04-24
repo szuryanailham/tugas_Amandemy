2001b0c4a73d4860c6049ec7b0a3e2362b2ee4c4
ilham Suryana <szuryanailham090102@gmail.com>
2024-04-24 19:21:59 +0700

    tugas laravel 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/list_menu/{makanan}/{harga}', function ($makanan, $harga) {
+    $menu = [
+        [
+            'makanan' => 'soto ayam',
+            'harga' => 8000
+        ],
+        [
+            'makanan' => 'bakso malang',
+            'harga' => 13000
+        ],
+        [
+            'makanan' => 'ketoprak',
+            'harga' => 15000
+        ]
+    ];
+
+    $foundMenu = collect($menu)->firstWhere('makanan', $makanan);
+    if ($foundMenu && $foundMenu['harga'] == $harga) {
+        return view('welcome',[
+            
+            'info' => 'data berhasil didapat',
+            'makanan' => $foundMenu,
+            'data' =>$menu
+        ]);
+    } else {
+        return response()->json(['info' => 'Menu tidak ditemukan'], 404);
+    }
 });
